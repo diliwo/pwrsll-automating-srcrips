2cc7e1071f7c3d5f60f7475ba90c3786cd41f250
Hmbilo <101592152+diliwo@users.noreply.github.com>
2024-07-06 18:11:57 +0200

    Add web app script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,4 +9,18 @@ function push-api-projects( $project ){
     invoke-item *.sln;
 }
 
+function push-app-projects( $project ){
+    # 1 : navigate powershell to the project folder
+    pushd "D:\WORKSPACES\SILVER\Azure-1030\WebApps\$project"
+
+    # 2 : open windows explorer at the project folder
+    invoke-item .;
+
+    # 3 : open any solution found in the project folder
+    code .;
+}
+
+
 New-Alias -Name oapi -value push-api-projects
+
+New-Alias -Name oapp -value push-app-projects
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SCript location
 File must be located in C:\Users\hmbilo\Documents\PowerShell folder.
+
+# For api project execute:
+
+home> oapi isp-web-api  
+
+# For app project execute:
+
+home> oapp appi-web-app  
