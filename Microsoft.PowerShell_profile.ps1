1bbcefd4c9b313c29112322aa20c88d0abfe3739
Hmbilo <101592152+diliwo@users.noreply.github.com>
2024-07-06 16:45:40 +0200

    Add push api project fonction

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+function push-api-projects( $project ){
+    # 1 : navigate powershell to the project folder
+    pushd "D:\WORKSPACES\SILVER\Azure-1030\WebApis\$project"
+
+    # 2 : open windows explorer at the project folder
+    invoke-item .;
+
+    # 3 : open any solution found in the project folder
+    invoke-item *.sln;
+}
+
+New-Alias -Name oapi -value push-api-projects
---
+++ README.md
@@ -0,0 +1,2 @@
+# SCript location
+File must be located in C:\Users\hmbilo\Documents\PowerShell folder.
